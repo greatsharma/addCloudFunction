3b0084508ba3364477f27422d5c76c8d9b22f272
Gaurav Sharma <greatsharma04@gmail.com>
2019-12-22 12:58:16 +0530

    add api

---
+++ api/func1.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+# @app.route('/<path:path>')
+# @app.route('/', defaults={'path': ''})
+@app.route('/api/', methods=['GET'])
+def add():
+    try:
+        num1 = int(request.args.get('num1'))
+        num2 = int(request.args.get('num2'))
+
+        response = {
+            'num1': num1,
+            'num2': num2,
+            'sum': num1 + num2
+        }
+    except Exception:
+        response = {
+            'status': {
+                'code': 400,
+                'msg': 'Bad request! specify both numbers'
+            }
+        }
+
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
